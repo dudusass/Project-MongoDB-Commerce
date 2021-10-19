fc6abdd9bed1abee6291a8c8637241c762be7eb4
Sass <dudu_sass12@hotmail.com>
2021-10-18 22:26:27 -0300

    requisitos 19 e 20

---
+++ challenges/desafio19.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  {},
+  { $rename: { descricao: "descricaoSite" } },
+);
+
+db.produtos.find(
+  null,
+  {
+    _id:0, nome: 1, descricao: 1, descricaoSite: 1,
+  },
+);
---
+++ challenges/desafio20.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: null } },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id:0, nome: 1, curtidas: 1,
+  },
+);
